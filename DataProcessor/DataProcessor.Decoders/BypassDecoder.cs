4cf4bf4bb117c70e5944d50e8e71b724522a872c
Ernesto Valcarcel <valcarcel_perez@hotmail.com>
2020-05-29 06:52:57 -0400

    Added BypassDecoder.

---
+++ DataProcessor/DataProcessor.Decoders/BypassDecoder.cs
@@ -0,0 +1,12 @@
+﻿using DataProcessor.Domain.Models;
+
+namespace DataProcessor.Decoders
+{
+    public class BypassDecoder : FieldDecoder
+    {
+        public override void Decode(Field field)
+        {
+            field.ValidationResult = ValidationResultType.Valid;
+        }
+    }
+}
