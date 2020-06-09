b8f9ab3373a3f82c47ef67e018df0521475e5921
Ernesto Valcarcel <valcarcel_perez@hotmail.com>
2020-06-09 07:53:03 -0400

    Adding FieldAggregator.

---
+++ DataProcessor/DataProcessor.Domain/Contracts/IFieldAggregator.cs
@@ -0,0 +1,13 @@
+﻿using DataProcessor.Domain.Models;
+
+namespace DataProcessor.Domain.Contracts
+{
+    public interface IFieldAggregator
+    {
+        void Aggregate(Field field, Aggregate aggregate);
+        string Name { get; set; }
+        string Description { get; set; }
+        string Args { get; set; }
+        ValidationResultType? FailValidationResult { get; set; }
+    }
+}
---
+++ DataProcessor/DataProcessor.Domain/Models/Aggregate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DataProcessor.Domain.Models
+{
+    public class Aggregate
+    {
+        public string Name { get; set; }
+
+        public object Value { get; set; }
+
+        public int AsInt()
+        {
+            return Convert.ToInt32(Value);
+        }
+
+        public bool AsBool()
+        {
+            return Convert.ToBoolean(Value);
+        }
+
+        public decimal AsDecimal()
+        {
+            return Convert.ToDecimal(Value);
+        }
+
+        public DateTime AsDateTime()
+        {
+            return Convert.ToDateTime(Value);
+        }
+
+        public string AsText()
+        {
+            return Convert.ToString(Value);
+        }
+    }
+}
