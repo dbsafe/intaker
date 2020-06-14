1cf9a62ce13f7eecfee7913a036de8e278e64bbe
Ernesto Valcarcel <valcarcel_perez@hotmail.com>
2020-06-14 17:15:08 -0400

    Adding orchestrator FileDataLoader

---
+++ DataProcessor/DataProcessor.Domain/Contracts/IDataRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace DataProcessor.Domain.Contracts
+{
+    public interface IDataRepository
+    {
+        InitializeFileResult InitializeFile(InitializeFileRequest request);
+    }
+
+    public class InitializeFileResult
+    {
+
+    }
+
+    public class InitializeFileRequest
+    {
+
+    }
+}
---
+++ DataProcessor/DataProcessor/FileDataLoader.cs
@@ -0,0 +1,42 @@
+﻿using DataProcessor.DataSource.File;
+using DataProcessor.Domain.Contracts;
+
+namespace DataProcessor
+{
+    public class FileDataLoaderConfig
+    {
+        public string Delimiter { get; set; }
+        public bool HasFieldsEnclosedInQuotes { get; set; }
+    }
+
+    public class FileDataLoader
+    {
+        private readonly IDataRepository _dataRepository;
+        private readonly FileDataLoaderConfig _config;
+
+        public FileDataLoader(FileDataLoaderConfig config, IDataRepository dataRepository)
+        {
+            _config = config;
+            _dataRepository = dataRepository;
+        }
+
+        public void Load(string dataFilePath, string fileSpecPath)
+        {
+            var fileDataSource = CreateDataSource(dataFilePath);
+
+            // var parsedDataProcessor = new ParsedDataProcessor(fileDataSource);
+        }
+
+        private IDataSource CreateDataSource(string path)
+        {
+            var fileDataSourceConfig = new FileDataSourceConfig
+            {
+                Delimiter = _config.Delimiter,
+                HasFieldsEnclosedInQuotes = _config.HasFieldsEnclosedInQuotes,
+                Path = path
+            };
+
+            return new FileDataSource(fileDataSourceConfig);
+        }
+    }
+}
