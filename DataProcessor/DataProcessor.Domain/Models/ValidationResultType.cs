86bb2989ab72e8dbf58994fc504bbd0f8d81dc0d
valcarcelperez <valcarcel_perez@hotmail.com>
2020-10-25 11:54:58 -0400

    ValidationResultType - Added documentation. (#8)

--- DataProcessor/DataProcessor.Domain/Models/ValidationResultType.cs
+++ DataProcessor/DataProcessor.Domain/Models/ValidationResultType.cs
@@ -2,9 +2,24 @@
 {
     public enum ValidationResultType
     {
+        /// <summary>
+        /// Validation succeed.
+        /// </summary>
         Valid = 1,
+        
+        /// <summary>
+        /// Validation failed. Adds flexibility by treating some validation fails as warnings.
+        /// </summary>
         Warning = 2,
+
+        /// <summary>
+        /// Validation failed.
+        /// </summary>
         Error = 3,
+
+        /// <summary>
+        /// Validation failed. Causes the decoding process to abort.
+        /// </summary>
         Critical = 4
     }
 }
