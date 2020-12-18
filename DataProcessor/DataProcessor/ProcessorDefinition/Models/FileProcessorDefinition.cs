80909c374e64e457209602993cc0705f4837adad
Ernesto Valcarcel <valcarcel_perez@hotmail.com>
2020-12-18 08:15:19 -0500

    Moved classes to separate files.

--- DataProcessor/DataProcessor/ProcessorDefinition/Models/FileProcessorDefinition.cs
+++ DataProcessor/DataProcessor/ProcessorDefinition/Models/FileProcessorDefinition.cs
@@ -1,21 +1,9 @@
-﻿using System.Collections.Generic;
-
-namespace DataProcessor.ProcessorDefinition.Models
+﻿namespace DataProcessor.ProcessorDefinition.Models
 {
     public abstract class FileProcessorDefinition
     {
         public bool CreateRowJsonEnabled { get; set; }
         public RowProcessorDefinition HeaderRowProcessorDefinition { get; set; }
         public RowProcessorDefinition TrailerRowProcessorDefinition { get; set; }
-    }
-
-    public class FileProcessorDefinition10 : FileProcessorDefinition
-    {
-        public RowProcessorDefinition DataRowProcessorDefinition { get; set; }
-    }
-
-    public class FileProcessorDefinition20 : FileProcessorDefinition
-    {
-        public Dictionary<string, RowProcessorDefinition> DataRowProcessorDefinitions { get; set; }
     }
 }
---
+++ DataProcessor/DataProcessor/ProcessorDefinition/Models/FileProcessorDefinition20.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace DataProcessor.ProcessorDefinition.Models
+{
+    public class FileProcessorDefinition20 : FileProcessorDefinition
+    {
+        public string KeyField { get; set; }
+        public Dictionary<string, RowProcessorDefinition> DataRowProcessorDefinitions { get; set; }
+    }
+}
