c4a0e8d0919723b6b1ff7ae33f3a5b5a21bfd2b4
Ernesto Valcarcel <valcarcel_perez@hotmail.com>
2021-01-29 09:38:46 -0500

    ParsedDataProcessor20 - Added ParsedDataProcessor20Test_Header_Data.

--- DataProcessor/DataProcessor.Tests/ParsedDataProcessor20Test_Header_Data.cs
+++ DataProcessor/DataProcessor.Tests/ParsedDataProcessor20Test_Header_Data.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 namespace DataProcessor.Tests
 {
     [TestClass]
-    public class ParsedDataProcessor20Test_Data_Tailer
+    public class ParsedDataProcessor20Test_Header_Data
     {
         private FileProcessorDefinition20 _fileProcessorDefinition;
         private TextDecoder _textDecoder;
@@ -16,15 +16,14 @@ namespace DataProcessor.Tests
 
         private DataRowProcessorDefinition _dataType1;
         private DataRowProcessorDefinition _dataType2;
-        private RowProcessorDefinition _trailer;
+        private RowProcessorDefinition _header;
 
         [TestInitialize]
         public void Initialize()
         {
-            _fileDataSource = TestHelpers.CreateFileDataSource<ParserContext20>("test-file-data-trailer.20.csv", false);
+            _fileDataSource = TestHelpers.CreateFileDataSource<ParserContext20>("test-file-header-data.20.csv", false);
 
             _textDecoder = new TextDecoder { Pattern = @"*.", FailValidationResult = ValidationResultType.Critical };
-
             _dataType1 = new DataRowProcessorDefinition
             {
                 DataTypeFieldIndex = 0,
@@ -54,24 +53,24 @@ namespace DataProcessor.Tests
                 }
             };
 
-            _trailer = new RowProcessorDefinition
+            _header = new RowProcessorDefinition
             {
                 FieldProcessorDefinitions = new FieldProcessorDefinition[]
-                    {
-                        new FieldProcessorDefinition { Decoder = _textDecoder, FieldName = "Field-TA" },
-                        new FieldProcessorDefinition { Decoder = _textDecoder, FieldName = "Field-TB" }
-                    }
+                                {
+                        new FieldProcessorDefinition { Decoder = _textDecoder, FieldName = "Field-HA" },
+                        new FieldProcessorDefinition { Decoder = _textDecoder, FieldName = "Field-HB" }
+                                }
             };
 
             _fileProcessorDefinition = new FileProcessorDefinition20
             {
                 DataTypeField = "FieldA",
                 KeyField = "key-field",
-                HeaderRowProcessorDefinition = new RowProcessorDefinition
+                HeaderRowProcessorDefinition = _header,
+                TrailerRowProcessorDefinition = new RowProcessorDefinition
                 {
-                    FieldProcessorDefinitions = new FieldProcessorDefinition[] { },
+                    FieldProcessorDefinitions = new FieldProcessorDefinition[] { }
                 },
-                TrailerRowProcessorDefinition = _trailer,
                 DataRowProcessorDefinitions = new Dictionary<string, DataRowProcessorDefinition>
                 {
                     { "dt1", _dataType1 },
@@ -92,33 +91,33 @@ namespace DataProcessor.Tests
             Assert.AreEqual(0, actual.Errors.Count);
 
             var row0 = actual.AllRows[0];
-            Assert.AreEqual(3, row0.Fields.Count);
-            Assert.AreEqual("dt1", row0.Fields[0].Value);
-            Assert.AreEqual("key-value", row0.Fields[1].Value);
-            Assert.AreEqual("field-1c", row0.Fields[2].Value);
+            Assert.AreEqual(2, row0.Fields.Count);
+            Assert.AreEqual("h1", row0.Fields[0].Value);
+            Assert.AreEqual("h2", row0.Fields[1].Value);
 
             var row1 = actual.AllRows[1];
             Assert.AreEqual(3, row1.Fields.Count);
             Assert.AreEqual("dt1", row1.Fields[0].Value);
             Assert.AreEqual("key-value", row1.Fields[1].Value);
-            Assert.AreEqual("field-2c", row1.Fields[2].Value);
+            Assert.AreEqual("field-1c", row1.Fields[2].Value);
 
             var row2 = actual.AllRows[2];
-            Assert.AreEqual(4, row2.Fields.Count);
-            Assert.AreEqual("dt2", row2.Fields[0].Value);
-            Assert.AreEqual("field-3b", row2.Fields[1].Value);
-            Assert.AreEqual("key-value", row2.Fields[2].Value);
-            Assert.AreEqual("field-3d", row2.Fields[3].Value);
+            Assert.AreEqual(3, row2.Fields.Count);
+            Assert.AreEqual("dt1", row2.Fields[0].Value);
+            Assert.AreEqual("key-value", row2.Fields[1].Value);
+            Assert.AreEqual("field-2c", row2.Fields[2].Value);
 
             var row3 = actual.AllRows[3];
-            Assert.AreEqual(2, row3.Fields.Count);
-            Assert.AreEqual("t1", row3.Fields[0].Value);
-            Assert.AreEqual("t2", row3.Fields[1].Value);
+            Assert.AreEqual(4, row3.Fields.Count);
+            Assert.AreEqual("dt2", row3.Fields[0].Value);
+            Assert.AreEqual("field-3b", row3.Fields[1].Value);
+            Assert.AreEqual("key-value", row3.Fields[2].Value);
+            Assert.AreEqual("field-3d", row3.Fields[3].Value);
 
-            Assert.IsNull(actual.Header);
-            
-            Assert.IsNotNull(actual.Trailer);
-            Assert.AreEqual(row3, actual.Trailer);
+
+            Assert.IsNotNull(actual.Header);
+            Assert.AreEqual(row0, actual.Header);
+            Assert.IsNull(actual.Trailer);
         }
     }
 }
