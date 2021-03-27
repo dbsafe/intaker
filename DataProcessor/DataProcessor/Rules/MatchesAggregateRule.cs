b29cc604e60a021a0d02fbd98ff30de4d89aa272
Ernesto Valcarcel <valcarcel_perez@hotmail.com>
2021-03-26 21:55:18 -0400

    MatchesAggregateRule - Renaming.

--- DataProcessor/DataProcessor/Rules/MatchesAggregateRule.cs
+++ DataProcessor/DataProcessor/Rules/MatchesAggregateRule.cs
@@ -12,20 +12,20 @@ namespace DataProcessor.Rules
         private const string ARG_AGGREGATE = "AggregateName";
         private Aggregate _aggregateArg;
 
-        private void SetAggregate(IEnumerable<Aggregate> aggregates)
+        private void SetArg(IEnumerable<Aggregate> aggregates)
         {
-            var aggregateNameArg = Args?.FirstOrDefault(a => a.Key == ARG_AGGREGATE).Value;
-            if (string.IsNullOrEmpty(aggregateNameArg))
+            var textArg = Args?.FirstOrDefault(a => a.Key == ARG_AGGREGATE).Value;
+            if (string.IsNullOrEmpty(textArg))
             {
                 throw new InvalidOperationException($"Rule: '{Name}'. Argument '{ARG_AGGREGATE}' not found");
             }
 
-            DataProcessorGlobal.Debug($"Rule: {Name}. Argument {ARG_AGGREGATE}: '{aggregateNameArg}'.");
+            DataProcessorGlobal.Debug($"Rule: {Name}. Argument {ARG_AGGREGATE}: '{textArg}'.");
 
-            _aggregateArg = aggregates.FirstOrDefault(a => a.Name == aggregateNameArg);
+            _aggregateArg = aggregates.FirstOrDefault(a => a.Name == textArg);
             if (_aggregateArg == null)
             {
-                throw new InvalidOperationException($"Rule: '{Name}'. Aggregate '{aggregateNameArg}' not found");
+                throw new InvalidOperationException($"Rule: '{Name}'. Aggregate '{textArg}' not found");
             }
         }
 
@@ -46,7 +46,7 @@ namespace DataProcessor.Rules
         public override void Initialize(FieldRuleConfiguration config)
         {
             base.Initialize(config);
-            SetAggregate(config.Aggregates);
+            SetArg(config.Aggregates);
         }
     }
 }
